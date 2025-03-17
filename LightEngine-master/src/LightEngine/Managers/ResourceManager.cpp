7a5318ca335b6816042d57d448c816f18161c8b1
ColynCO <110816158+Sawful@users.noreply.github.com>
2025-03-17 16:52:16 +0100

    sound system in resource manager

--- LightEngine-master/src/LightEngine/Managers/ResourceManager.cpp
+++ LightEngine-master/src/LightEngine/Managers/ResourceManager.cpp
@@ -9,6 +9,13 @@ ResourceManager::ResourceManager() {
 	Resources::m_Resources = this;
 }
 
+ResourceManager::~ResourceManager()
+{
+	m_Textures.erase(m_Textures.begin(), m_Textures.end());
+	m_Fonts.erase(m_Fonts.begin(), m_Fonts.end());
+	m_SoundBuffers.erase(m_SoundBuffers.begin(), m_SoundBuffers.end());
+}
+
 void ResourceManager::loadTexture(std::string _name) {
 	addTexture(_name, OS::getFileInDirectory(TEMP_RES_PATH, _name + ".png"));
 }
@@ -31,12 +38,25 @@ sf::Texture* ResourceManager::getTexture(std::string _name) {
 void ResourceManager::loadFont(std::string _name) {
 	addTexture(_name, OS::getFileInDirectory(TEMP_RES_PATH, _name + ".ttf"));
 }
+
 void ResourceManager::unloadFont(std::string _name) {
 	m_Fonts.erase(_name);
 }
+
 void ResourceManager::addFont(std::string _name, std::string _path) {
 	bool success = m_Fonts[_name].loadFromFile(_path);
 }
+
+void ResourceManager::addSoundBuffer(std::string _name, std::string _path)
+{
+	bool success = m_SoundBuffers[_name].loadFromFile(_path);
+}
+
+void ResourceManager::addMusic(std::string _name, std::string _path)
+{
+	bool success = m_Musics[_name].openFromFile(_path);
+}
+
 sf::Font* ResourceManager::getFont(std::string _name) {
 	bool success = m_Fonts.find(_name) != m_Fonts.end();
 	if (!success) {
@@ -44,4 +64,44 @@ sf::Font* ResourceManager::getFont(std::string _name) {
 		success = m_Fonts.find(_name) != m_Fonts.end();
 	}
 	return &m_Fonts[_name];
+}
+
+void ResourceManager::loadSoundBuffer(std::string _name)
+{
+	addSoundBuffer(_name, OS::getFileInDirectory(TEMP_RES_PATH, _name + ".mp3"));
+}
+
+void ResourceManager::unloadSoundBuffer(std::string _name)
+{
+	m_SoundBuffers.erase(_name);
+}
+
+sf::SoundBuffer* ResourceManager::getSoundBuffer(std::string _name)
+{
+	bool success = m_SoundBuffers.find(_name) != m_SoundBuffers.end();
+	if (!success) {
+		loadFont(_name);
+		success = m_SoundBuffers.find(_name) != m_SoundBuffers.end();
+	}
+	return &m_SoundBuffers[_name];
+}
+
+void ResourceManager::loadMusic(std::string _name)
+{
+	addMusic(_name, OS::getFileInDirectory(TEMP_RES_PATH, _name + ".mp3"));
+}
+
+void ResourceManager::unloadMusic(std::string _name)
+{
+	m_Musics.erase(_name);
+}
+
+sf::Music* ResourceManager::getMusic(std::string _name)
+{
+	bool success = m_Musics.find(_name) != m_Musics.end();
+	if (!success) {
+		loadFont(_name);
+		success = m_Musics.find(_name) != m_Musics.end();
+	}
+	return &m_Musics[_name];
 }
--- LightEngine-master/src/LightEngine/Managers/ResourceManager.h
+++ LightEngine-master/src/LightEngine/Managers/ResourceManager.h
@@ -2,6 +2,7 @@
 
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Audio.hpp>
 #include <string>
 #include <map>
 
@@ -17,14 +18,27 @@ public:
 	void unloadFont(std::string _name);
 	sf::Font* getFont(std::string _name);
 
+	void loadSoundBuffer(std::string _name);
+	void unloadSoundBuffer(std::string _name);
+	sf::SoundBuffer* getSoundBuffer(std::string _name);
+
+	void loadMusic(std::string _name);
+	void unloadMusic(std::string _name);
+	sf::Music* getMusic(std::string _name);
+
 private:
 	ResourceManager();
+	~ResourceManager();
 
 	std::map<std::string, sf::Texture> m_Textures;
 	std::map<std::string, sf::Font> m_Fonts;
+	std::map<std::string, sf::SoundBuffer> m_SoundBuffers;
+	std::map<std::string, sf::Music> m_Musics;
 
 	void addTexture(std::string _name, std::string _path);
 	void addFont(std::string _name, std::string _path);
+	void addSoundBuffer(std::string _name, std::string _path);
+	void addMusic(std::string _name, std::string _path);
 
 	friend class Managers;
 };
